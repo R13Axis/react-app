4fde9d01596ee9b2c44dadcfb0e2781cd4429dd7
R13Axis <rafael-alam@hotmail.com>
2022-12-17 13:58:34 -0600

    agrege archivos

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-import 'style.css'
+import './styles.css'
+import search from './assets/buscar.png'
 
-const Navbar = ({children}) => {
+const Navbar = () => {
     return (
-        <div>
+        <div className="navbar">
+
+            <div className="brand">
+
+                <h1>Gaming Cloud</h1>
+
+            </div>
+
+            <div className="menu">
+                <ul className="menu-list">
+                    <li className="item">Inicio</li>
+                    <li className="item">Accesorios</li>
+                    <li className="item">Consumibles</li>
+                    <li className="item">Nosotros</li>
+                    <li className="item">Contacto</li>
+                </ul>
+            </div>
+
+            <div className="icons">
+
+                <img src={search} alt="search icon" ></img>
+
+            </div>
             
         </div>
     )
 }
+
+export default Navbar
